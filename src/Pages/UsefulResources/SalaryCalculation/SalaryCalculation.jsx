0aeff0ec865ffcd2eb0efdb33542a492394f91ce
EdgarGrigoryan1996 <footballistna@gmail.com>
2023-05-26 16:35:08 +0400

    added salary calculation

--- src/Pages/UsefulResources/SalaryCalculation/SalaryCalculation.jsx
+++ src/Pages/UsefulResources/SalaryCalculation/SalaryCalculation.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from "./SalaryCalculation.module.css"
-
+import g from "../../../globalStyles.module.css"
 function SalaryCalculation(props) {
+    const [registeredMethod, setRegisteredMethod] = useState(true)
+    const [payMethod, setPayMethod] = useState(true)
+    const [currentSalary, setCurrentSalary] = useState(0)
+
     return (
-        <div>SalaryCalculation</div>
+        <section className={s.salaryCalculate}>
+            <div className={g.sectionTitle}>
+                <h2>Salary Calculation</h2>
+            </div>
+            <div className={s.calculateBlock}>
+
+                <div className={s.cleanRegisteredBlock}>
+                    <div className={(registeredMethod ? s.selectedMethod : "")} onClick={() => setRegisteredMethod(true)}>Գրանցված</div>
+                    <div className={(!registeredMethod ? s.selectedMethod : "")} onClick={() => setRegisteredMethod(false)}>Մաքուր</div>
+                </div>
+
+                <div className={s.vcharoxBlock}>
+                    <div className={(payMethod ? s.selectedMethod : "")} onClick={() => setPayMethod(true)}>Yes</div>
+                    <div className={(!payMethod ? s.selectedMethod : "")} onClick={() => setPayMethod(false)}>No</div>
+                </div>
+
+                <div className={s.enterSalary}>
+                    <input type="text" value={currentSalary} onChange={(e) => {
+                        const currentSymbol = e.target.value
+                        if(currentSymbol.toLowerCase() === currentSymbol.toLocaleUpperCase()){
+                            setCurrentSalary(e.target.value)
+                        }
+                    }}/>
+                </div>
+                <div className={s.resultWrapper}>
+                    <div className={s.resultBlock}>
+                        <div>Եկամտահարկ 20% / ՏՏ 10%	</div>
+                        <div>{currentSalary}</div>
+                    </div>
+                    <div className={s.resultBlock}>
+                        <div>Սոցիալական վճար</div>
+                        <div>{currentSalary}</div>
+                    </div>
+                    <div className={s.resultBlock}>
+                        <div>Դրոշմանիշային վճար</div>
+                        <div>{currentSalary}</div>
+                    </div>
+                    <div className={s.resultBlock}>
+                        <div>Պետք է գրանցել</div>
+                        <div>{currentSalary}</div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
