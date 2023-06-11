6d2338db32d3a526e5175a2e112dcdc4e8a4fe07
EdgarGrigoryan1996 <grigoryan.edgar.1996@gmail.com>
2023-06-11 18:35:16 +0400

    test

--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ import './App.css';
 import {useTranslation} from "react-i18next";
 import TopInformationSection from './components/TopInformationSection/TopInformationSection';
 import Menu from './components/Menu/Menu';
-import Header from "./components/Header/Header";
-import Industries from "./components/Industries/Industries";
-import HeartbeatMiniSection from "./components/HeartbeatMiniSection/HeartbeatMiniSection";
+
 import Home from "./Pages/Home/Home";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import AboutCompany from "./Pages/Company/AboutCompany/AboutCompany";
 import Partners from "./Pages/Company/Partners/Partners";
 import Staff from "./Pages/Company/Staff/Staff";
-import BusinessConsulting from "./Pages/Services/BusinessConsulting/BusinessConsulting";
-import ConsultancyAccounting from "./Pages/Services/ConsultancyAccounting/ConsultancyAccounting";
-import ConsultancyTax from "./Pages/Services/ConsultancyTax/ConsultancyTax";
-import Courses from "./Pages/Services/Courses/Courses";
-import Hr from "./Pages/Services/Hr/Hr";
-import Accounting from "./Pages/Services/Accounting/Accounting";
 import Links from "./Pages/UsefulResources/Links/Links";
 import SalaryCalculation from "./Pages/UsefulResources/SalaryCalculation/SalaryCalculation";
 import Standards from "./Pages/UsefulResources/Standards/Standards";
@@ -32,10 +24,8 @@ import hrImg from "./assets/images/services/hr.jpg"
 import Price from './Pages/Services/Price/Price';
 import WithScrollTop from "./HOC/WithScrollTop";
 
-
-
 function App() {
-  const {t,i18n} = useTranslation()
+  const {t} = useTranslation()
 
     const servicePagesData = {
         "accounting": {
@@ -109,34 +99,29 @@ function App() {
 
   return (
     <div className="App">
-
         <TopInformationSection />
         <Menu />
-
-        
-    <div className='content'>
-    <Routes>
-                <Route path={"/"} element={<Home />}/>
-                <Route path={"/about"} element={<WithScrollTop Child={AboutCompany} />}/>
-                <Route path={"/partners"} element={<WithScrollTop Child={Partners} /> }/>
-                <Route path={"/staff"} element={<WithScrollTop Child={Staff} />}/>
-                <Route path={"/businessConsulting"} element={<ServicePagesTemplate data={servicePagesData.planning} image={planningImg}/>}/>
-                {/*<Route path={"/consultancyAccounting"} element={<ConsultancyAccounting />}/>*/}
-                <Route path={"/consultancyTax"} element={<ServicePagesTemplate data={servicePagesData.tax} image={taxImg}/>}/>
-                <Route path={"/courses"} element={<ServicePagesTemplate data={servicePagesData.trainings} image={trainingsImg}/>}/>
-                <Route path={"/hr"} element={<ServicePagesTemplate data={servicePagesData.hr} image={hrImg}/>}/>
-                <Route path={"/management"} element={<ServicePagesTemplate data={servicePagesData.accounting} image={accountingImg}/>}/>
-                <Route path={"/price"} element={<WithScrollTop Child={Price} />}/>
-                <Route path={"/links"} element={<WithScrollTop Child={Links} />}/>
-                <Route path={"/salaryCalculation"} element={<WithScrollTop Child={SalaryCalculation} />}/>
-                <Route path={"/loanCalculation"} element={<WithScrollTop Child={LoanCalculator} />}/>
-                <Route path={"/standards"} element={<WithScrollTop Child={Standards} />} />
-                <Route path={"*"} element={<WithScrollTop Child={NotFound} />}/>
-            </Routes>
-            <Footer />
-    </div>
-            
-            
+        <div className='content'>
+        <Routes>
+                    <Route path={"/"} element={<Home />}/>
+                    <Route path={"/about"} element={<WithScrollTop Child={AboutCompany} />}/>
+                    <Route path={"/partners"} element={<WithScrollTop Child={Partners} /> }/>
+                    <Route path={"/staff"} element={<WithScrollTop Child={Staff} />}/>
+                    <Route path={"/businessConsulting"} element={<ServicePagesTemplate data={servicePagesData.planning} image={planningImg}/>}/>
+                    {/*<Route path={"/consultancyAccounting"} element={<ConsultancyAccounting />}/>*/}
+                    <Route path={"/consultancyTax"} element={<ServicePagesTemplate data={servicePagesData.tax} image={taxImg}/>}/>
+                    <Route path={"/courses"} element={<ServicePagesTemplate data={servicePagesData.trainings} image={trainingsImg}/>}/>
+                    <Route path={"/hr"} element={<ServicePagesTemplate data={servicePagesData.hr} image={hrImg}/>}/>
+                    <Route path={"/management"} element={<ServicePagesTemplate data={servicePagesData.accounting} image={accountingImg}/>}/>
+                    <Route path={"/price"} element={<WithScrollTop Child={Price} />}/>
+                    <Route path={"/links"} element={<WithScrollTop Child={Links} />}/>
+                    <Route path={"/salaryCalculation"} element={<WithScrollTop Child={SalaryCalculation} />}/>
+                    <Route path={"/loanCalculation"} element={<WithScrollTop Child={LoanCalculator} />}/>
+                    <Route path={"/standards"} element={<WithScrollTop Child={Standards} />} />
+                    <Route path={"*"} element={<WithScrollTop Child={NotFound} />}/>
+                </Routes>
+                <Footer />
+        </div>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import s from "./Header.module.css"
 function Header(props) {
     return (
-        <div className={s.headerWrapper}>
-            <div className={s.background}>
-                <div className={s.headerContentBlock}>
-                    <div><h4>Keeping your web presence</h4></div>
-                    <div><h1>Accounting & Finances</h1></div>
-                    <div><p>However big the scale of your business operations</p></div>
-                </div>
-            </div>
-        </div>
+        <div>Test</div>
+        // <div className={s.headerWrapper}>
+        //     <div className={s.background}>
+        //         <div className={s.headerContentBlock}>
+        //             <div><h4>Keeping your web presence</h4></div>
+        //             <div><h1>Accounting & Finances</h1></div>
+        //             <div><p>However big the scale of your business operations</p></div>
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
 
