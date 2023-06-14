4c46c18de597bbde025858cbb38cb83766276a31
EdgarGrigoryan1996 <grigoryan.edgar.1996@gmail.com>
2023-06-15 03:24:50 +0400

    test

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -24,7 +24,7 @@ function Menu() {
     return (
       <div className={s.menuWrapper}>
         <div className={s.menuBlock}>
-            <img src={logo} alt="Tever-Group"/>
+        <div className={s.logo}><Link to={"/"}><img src={logo} alt=""/>TEVER LLC</Link> </div>
             <div className={s.menu}>
                 <nav>
                 <div className={s.burgerMenu + " " + (burgerMenuStatus && s.menuOpened)} onClick={ () => setBurgerMenuStatus(!burgerMenuStatus) }><IoIosArrowForward /></div>
