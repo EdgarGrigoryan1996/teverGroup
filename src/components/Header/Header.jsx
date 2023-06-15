93f66259f2853a8d4d8268f0a36789050ddef36c
EdgarGrigoryan1996 <grigoryan.edgar.1996@gmail.com>
2023-06-15 09:49:44 +0400

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import s from "./Header.module.css"
 import {useTranslation} from "react-i18next";
+import headerImg from "../../assets/images/headerImg-min.webp"
 function Header() {
     const {t} = useTranslation()
     return (
         <div className={s.headerWrapper}>
+            <img src={headerImg} alt=""/>
+            <div className={s.headerContentBlock}>
+                <div><h1>{t("header.title")}</h1></div>
+                <div><p>{t("header.subTitle")}</p></div>
+            </div>
             <div className={s.background}>
-                <div className={s.headerContentBlock}>
-                    <div><h1>{t("header.title")}</h1></div>
-                    <div><p>{t("header.subTitle")}</p></div>
-                </div>
+
+
             </div>
         </div>
     );
