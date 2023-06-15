84c7ec7126acd4700e02214659929b08f92ebe13
EdgarGrigoryan1996 <grigoryan.edgar.1996@gmail.com>
2023-06-15 09:37:48 +0400

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,14 @@ import {useTranslation} from "react-i18next";
 function Header() {
     const {t} = useTranslation()
     return (
-        <div></div>
+        <div className={s.headerWrapper}>
+            <div className={s.background}>
+                <div className={s.headerContentBlock}>
+                    <div><h1>{t("header.title")}</h1></div>
+                    <div><p>{t("header.subTitle")}</p></div>
+                </div>
+            </div>
+        </div>
     );
 }
 
