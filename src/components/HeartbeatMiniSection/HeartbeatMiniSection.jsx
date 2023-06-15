21d06455c4994c3adf0eef57dd832201c5008dde
EdgarGrigoryan1996 <grigoryan.edgar.1996@gmail.com>
2023-06-15 09:24:02 +0400

    test

--- src/components/HeartbeatMiniSection/HeartbeatMiniSection.jsx
+++ src/components/HeartbeatMiniSection/HeartbeatMiniSection.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import s from "./HeartbeatMiniSection.module.css"
 function HeartbeatMiniSection() {
     return (
-        <div className={s.heartbeatWrapper}>
-            <div className={s.background}>
-
-            </div>
-        </div>
+        <div></div>
     );
 }
 
